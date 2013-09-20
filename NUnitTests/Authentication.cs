0e0c595c206f8aa38feb394aedce3a5457f87027
mupcakes <mupto1@gmail.com>
2013-09-20 13:06:19 -0400

    added comments

--- NUnitTests/Authentication.cs
+++ NUnitTests/Authentication.cs
@@ -5,37 +5,47 @@ using StormOnDemandAPI;
 
 namespace Tests
 {
+	/// <summary>
+	/// Tests that you able to set Auth info, doesn't touch the API
+	/// </summary>
 	[TestFixture]
 	public class Authentication
 	{
-		// variables
+		/// <summary>
+		/// user,password variables used in test
+		/// </summary>
 		string _testUser;
 		string _testPassword;
 
 		[SetUp]
 		public void Init ()
 		{
-			//sets up test data
+			/// <summary>
+			/// Data used to compare with test
+			/// </summary>
 			_testUser = "NUnitUser";
 			_testPassword = "NUnitPassword";
 			Auth.username = _testUser;
 			Auth.password = _testPassword;
 		}
 
+		/// <summary>
+		/// Determines whether this instance can set auth info.
+		/// </summary>
 		[Test]
 		public void CanSetAuthInfo()
 		{
-			// Just makes sure its able to set the auth info
 			StringAssert.AreEqualIgnoringCase (_testUser, Auth.username);
 			StringAssert.AreEqualIgnoringCase (_testPassword, Auth.password);
 		}
 
+		/// <summary>
+		/// Checks the type of the credentials.
+		/// </summary>
 		[Test]
 		public void CheckCredentialsType()
 		{
-			// checks that the auth credentials are the correct type
 			Assert.IsInstanceOf<ICredentials>(Auth.Credentials);
-
 		}
 	}
 }
