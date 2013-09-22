f19186545e37bc0c04fe09efe4476d0c5926d26d
mupcakes <mupto1@gmail.com>
2013-09-21 21:43:59 -0400

    updated comments

--- API/APIMethods/Product.cs
+++ API/APIMethods/Product.cs
@@ -31,35 +31,69 @@ namespace APIMethods
 {
 	public static class Product
 	{
+		/// <summary>
+		/// Returns API documentation on a specific method in Products/
+		/// </summary>
 		public static JObject MethodInfo (string method)
 		{
 			 return Documentation.docs["Product"]["__methods"][method];
 		}
 
+		/// <summary>
+		/// Returns information about a product's pricing and options.
+		/// The product code for a specific server or service on an account can be found with the
+		/// Server/Details method, under the 'type' field, and a list of valid product codes can
+		/// be obtained via the Product/List method.
+		/// </summary>
 		public static string Details (object options, EncodeType encoding = EncodeType.JSON)
 		{
 			string method = "/Product/details";
 			return APIHandler.Post (method, options, encoding);
 		}
 
+		/// <summary>
+		/// Converts path elements to a product code and alias.
+		/// </summary>
 		public static string GetProductCodeFromPath (object options, EncodeType encoding = EncodeType.JSON)
 		{
 			string method = "/Product/getProductCodeFromPath";
 			return APIHandler.Post (method, options, encoding);
 		}
 
+		/// <summary>
+		/// Returns production information for all products, or products in a series or
+		/// category depending on the arguments passed. Only returns products available to the account.
+		/// </summary>
 		public static string List (object options, EncodeType encoding = EncodeType.JSON)
 		{
 			string method = "/Product/list";
 			return APIHandler.Post (method, options, encoding);
 		}
 
+		/// <summary>
+		/// Get a total price for a product, including other useful information that
+		/// will affect the order.
+		/// </summary>
+		/// <returns>
+		/// The 'balance' is the amount of money on the account that will be used prior to
+		/// attempting to charge your credit card for the server creation.
+		/// The 'initial' amount is the amount you need to prepay for the server for now
+		/// until your next billing date.
+		/// The 'cc_chage' amount is the amount that will be charged to your credit card,
+		/// if your account is set to the credit card payment method.
+		/// 'approved' is a boolean.  If true then this purchase would be allowed by the
+		/// system.
+		/// </returns>
 		public static string Price (object options, EncodeType encoding = EncodeType.JSON)
 		{
 			string method = "/Product/price";
 			return APIHandler.Post (method, options, encoding);
 		}
 
+		/// <summary>
+		/// Returns the minimal price for a product, picking the cheapest feature for
+		/// each slot.
+		/// </summary>
 		public static string StartingPrice (object options, EncodeType encoding = EncodeType.JSON)
 		{
 			string method = "/Product/startingPrice";
